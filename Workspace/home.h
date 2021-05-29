1c5d6ed031bfe5facc14c85e2e6a238f0fb2b6d5
Ravi Kumar <ravikuk@outlook.com>
2021-05-29 18:36:40 +0530

    Added new Dialogbox

--- Workspace/home.h
+++ Workspace/home.h
@@ -22,6 +22,7 @@ namespace Workspace {
 			//TODO: Add the constructor code here
 			//
 			main->Hide();
+			newWSDialog->Hide();
 		}
 
 	protected:
@@ -45,7 +46,19 @@ namespace Workspace {
 	private: System::Windows::Forms::TextBox^ textBox1;
 	private: System::Windows::Forms::Button^ button2;
 	private: System::Windows::Forms::Button^ button1;
-	private: System::Windows::Forms::ListBox^ listBox1;
+
+	private: System::Windows::Forms::Button^ button3;
+	private: System::Windows::Forms::TreeView^ treeView1;
+	private: System::Windows::Forms::Label^ label3;
+	private: System::Windows::Forms::Panel^ newWSDialog;
+	private: System::Windows::Forms::Button^ saveButton;
+
+	private: System::Windows::Forms::Button^ cancelBox;
+	private: System::Windows::Forms::TextBox^ tbWorkSpaceName;
+
+
+
+	private: System::Windows::Forms::Label^ label4;
 	protected:
 
 	protected:
@@ -80,13 +93,21 @@ namespace Workspace {
 			this->startButton = (gcnew System::Windows::Forms::Button());
 			this->pictureBox1 = (gcnew System::Windows::Forms::PictureBox());
 			this->main = (gcnew System::Windows::Forms::Panel());
+			this->newWSDialog = (gcnew System::Windows::Forms::Panel());
+			this->saveButton = (gcnew System::Windows::Forms::Button());
+			this->cancelBox = (gcnew System::Windows::Forms::Button());
+			this->tbWorkSpaceName = (gcnew System::Windows::Forms::TextBox());
+			this->label4 = (gcnew System::Windows::Forms::Label());
+			this->treeView1 = (gcnew System::Windows::Forms::TreeView());
+			this->label3 = (gcnew System::Windows::Forms::Label());
+			this->button3 = (gcnew System::Windows::Forms::Button());
 			this->label2 = (gcnew System::Windows::Forms::Label());
 			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
 			this->button2 = (gcnew System::Windows::Forms::Button());
 			this->button1 = (gcnew System::Windows::Forms::Button());
-			this->listBox1 = (gcnew System::Windows::Forms::ListBox());
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->BeginInit();
 			this->main->SuspendLayout();
+			this->newWSDialog->SuspendLayout();
 			this->SuspendLayout();
 			// 
 			// close
@@ -153,26 +174,131 @@ namespace Workspace {
 			// 
 			// main
 			// 
-			this->main->BackColor = System::Drawing::Color::Gainsboro;
+			this->main->BackColor = System::Drawing::Color::Transparent;
 			this->main->BackgroundImage = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"main.BackgroundImage")));
+			this->main->Controls->Add(this->newWSDialog);
+			this->main->Controls->Add(this->treeView1);
+			this->main->Controls->Add(this->label3);
+			this->main->Controls->Add(this->button3);
 			this->main->Controls->Add(this->label2);
 			this->main->Controls->Add(this->textBox1);
 			this->main->Controls->Add(this->button2);
 			this->main->Controls->Add(this->button1);
-			this->main->Controls->Add(this->listBox1);
 			this->main->Dock = System::Windows::Forms::DockStyle::Fill;
 			this->main->Location = System::Drawing::Point(0, 0);
 			this->main->Name = L"main";
 			this->main->Size = System::Drawing::Size(900, 600);
 			this->main->TabIndex = 4;
 			// 
+			// newWSDialog
+			// 
+			this->newWSDialog->BackColor = System::Drawing::Color::Black;
+			this->newWSDialog->Controls->Add(this->saveButton);
+			this->newWSDialog->Controls->Add(this->cancelBox);
+			this->newWSDialog->Controls->Add(this->tbWorkSpaceName);
+			this->newWSDialog->Controls->Add(this->label4);
+			this->newWSDialog->Location = System::Drawing::Point(261, 200);
+			this->newWSDialog->Name = L"newWSDialog";
+			this->newWSDialog->Size = System::Drawing::Size(400, 193);
+			this->newWSDialog->TabIndex = 8;
+			// 
+			// saveButton
+			// 
+			this->saveButton->BackColor = System::Drawing::Color::Blue;
+			this->saveButton->FlatStyle = System::Windows::Forms::FlatStyle::Flat;
+			this->saveButton->ForeColor = System::Drawing::Color::White;
+			this->saveButton->Location = System::Drawing::Point(167, 134);
+			this->saveButton->Name = L"saveButton";
+			this->saveButton->Size = System::Drawing::Size(93, 34);
+			this->saveButton->TabIndex = 3;
+			this->saveButton->Text = L"Save";
+			this->saveButton->UseVisualStyleBackColor = false;
+			this->saveButton->Click += gcnew System::EventHandler(this, &home::button5_Click);
+			// 
+			// cancelBox
+			// 
+			this->cancelBox->FlatAppearance->BorderColor = System::Drawing::Color::White;
+			this->cancelBox->FlatStyle = System::Windows::Forms::FlatStyle::Flat;
+			this->cancelBox->ForeColor = System::Drawing::Color::White;
+			this->cancelBox->Location = System::Drawing::Point(277, 134);
+			this->cancelBox->Name = L"cancelBox";
+			this->cancelBox->Size = System::Drawing::Size(85, 34);
+			this->cancelBox->TabIndex = 2;
+			this->cancelBox->Text = L"Cancel";
+			this->cancelBox->UseVisualStyleBackColor = true;
+			this->cancelBox->Click += gcnew System::EventHandler(this, &home::button4_Click);
+			// 
+			// tbWorkSpaceName
+			// 
+			this->tbWorkSpaceName->BackColor = System::Drawing::Color::Black;
+			this->tbWorkSpaceName->Font = (gcnew System::Drawing::Font(L"Segoe UI Semibold", 12, System::Drawing::FontStyle::Bold));
+			this->tbWorkSpaceName->ForeColor = System::Drawing::Color::White;
+			this->tbWorkSpaceName->Location = System::Drawing::Point(31, 73);
+			this->tbWorkSpaceName->Name = L"tbWorkSpaceName";
+			this->tbWorkSpaceName->Size = System::Drawing::Size(331, 34);
+			this->tbWorkSpaceName->TabIndex = 1;
+			this->tbWorkSpaceName->TextChanged += gcnew System::EventHandler(this, &home::textBox2_TextChanged);
+			// 
+			// label4
+			// 
+			this->label4->AutoSize = true;
+			this->label4->Font = (gcnew System::Drawing::Font(L"Segoe UI Semibold", 12, System::Drawing::FontStyle::Bold));
+			this->label4->ForeColor = System::Drawing::Color::White;
+			this->label4->Location = System::Drawing::Point(12, 14);
+			this->label4->Name = L"label4";
+			this->label4->Size = System::Drawing::Size(158, 28);
+			this->label4->TabIndex = 0;
+			this->label4->Text = L"New Workspace";
+			// 
+			// treeView1
+			// 
+			this->treeView1->BorderStyle = System::Windows::Forms::BorderStyle::FixedSingle;
+			this->treeView1->Font = (gcnew System::Drawing::Font(L"Segoe UI Semibold", 12, System::Drawing::FontStyle::Bold));
+			this->treeView1->Location = System::Drawing::Point(19, 88);
+			this->treeView1->Name = L"treeView1";
+			this->treeView1->Size = System::Drawing::Size(281, 424);
+			this->treeView1->TabIndex = 7;
+			this->treeView1->AfterSelect += gcnew System::Windows::Forms::TreeViewEventHandler(this, &home::treeView1_AfterSelect);
+			// 
+			// label3
+			// 
+			this->label3->AutoSize = true;
+			this->label3->BackColor = System::Drawing::Color::Transparent;
+			this->label3->Font = (gcnew System::Drawing::Font(L"Segoe UI Semibold", 15, System::Drawing::FontStyle::Bold));
+			this->label3->ForeColor = System::Drawing::Color::White;
+			this->label3->Location = System::Drawing::Point(25, 22);
+			this->label3->Name = L"label3";
+			this->label3->Size = System::Drawing::Size(150, 35);
+			this->label3->TabIndex = 6;
+			this->label3->Text = L"Workspaces";
+			// 
+			// button3
+			// 
+			this->button3->BackColor = System::Drawing::Color::Transparent;
+			this->button3->BackgroundImage = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"button3.BackgroundImage")));
+			this->button3->BackgroundImageLayout = System::Windows::Forms::ImageLayout::Stretch;
+			this->button3->FlatAppearance->BorderColor = System::Drawing::Color::Black;
+			this->button3->FlatAppearance->BorderSize = 0;
+			this->button3->FlatAppearance->MouseDownBackColor = System::Drawing::Color::Transparent;
+			this->button3->FlatAppearance->MouseOverBackColor = System::Drawing::Color::Transparent;
+			this->button3->FlatStyle = System::Windows::Forms::FlatStyle::Flat;
+			this->button3->ForeColor = System::Drawing::Color::Transparent;
+			this->button3->Location = System::Drawing::Point(861, 19);
+			this->button3->Margin = System::Windows::Forms::Padding(3, 10, 10, 3);
+			this->button3->Name = L"button3";
+			this->button3->Size = System::Drawing::Size(20, 20);
+			this->button3->TabIndex = 5;
+			this->button3->UseMnemonic = false;
+			this->button3->UseVisualStyleBackColor = false;
+			this->button3->Click += gcnew System::EventHandler(this, &home::button3_Click);
+			// 
 			// label2
 			// 
 			this->label2->AutoSize = true;
 			this->label2->BackColor = System::Drawing::Color::Transparent;
 			this->label2->Font = (gcnew System::Drawing::Font(L"Segoe UI Semibold", 15, System::Drawing::FontStyle::Bold));
-			this->label2->ForeColor = System::Drawing::Color::Black;
-			this->label2->Location = System::Drawing::Point(332, 22);
+			this->label2->ForeColor = System::Drawing::Color::White;
+			this->label2->Location = System::Drawing::Point(374, 22);
 			this->label2->Name = L"label2";
 			this->label2->Size = System::Drawing::Size(173, 35);
 			this->label2->TabIndex = 4;
@@ -188,30 +314,22 @@ namespace Workspace {
 			// 
 			// button2
 			// 
-			this->button2->Location = System::Drawing::Point(192, 534);
+			this->button2->Location = System::Drawing::Point(170, 534);
 			this->button2->Name = L"button2";
-			this->button2->Size = System::Drawing::Size(145, 53);
+			this->button2->Size = System::Drawing::Size(130, 53);
 			this->button2->TabIndex = 2;
-			this->button2->Text = L"button2";
+			this->button2->Text = L"New Item";
 			this->button2->UseVisualStyleBackColor = true;
 			// 
 			// button1
 			// 
 			this->button1->Location = System::Drawing::Point(19, 534);
 			this->button1->Name = L"button1";
-			this->button1->Size = System::Drawing::Size(167, 53);
+			this->button1->Size = System::Drawing::Size(145, 53);
 			this->button1->TabIndex = 1;
-			this->button1->Text = L"button1";
+			this->button1->Text = L"New Workspace";
 			this->button1->UseVisualStyleBackColor = true;
-			// 
-			// listBox1
-			// 
-			this->listBox1->FormattingEnabled = true;
-			this->listBox1->ItemHeight = 17;
-			this->listBox1->Location = System::Drawing::Point(19, 22);
-			this->listBox1->Name = L"listBox1";
-			this->listBox1->Size = System::Drawing::Size(281, 497);
-			this->listBox1->TabIndex = 0;
+			this->button1->Click += gcnew System::EventHandler(this, &home::button1_Click_1);
 			// 
 			// home
 			// 
@@ -233,6 +351,8 @@ namespace Workspace {
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->EndInit();
 			this->main->ResumeLayout(false);
 			this->main->PerformLayout();
+			this->newWSDialog->ResumeLayout(false);
+			this->newWSDialog->PerformLayout();
 			this->ResumeLayout(false);
 			this->PerformLayout();
 
@@ -252,6 +372,30 @@ private: System::Void pictureBox1_Click(System::Object^ sender, System::EventArg
 }
 private: System::Void startButton_Click(System::Object^ sender, System::EventArgs^ e) {
 	main->Show();
+}
+private: System::Void button3_Click(System::Object^ sender, System::EventArgs^ e) {
+	Application::Exit();
+}
+private: System::Void treeView1_AfterSelect(System::Object^ sender, System::Windows::Forms::TreeViewEventArgs^ e) {
+}
+private: System::Void button1_Click_1(System::Object^ sender, System::EventArgs^ e) {
+	newWSDialog->Show();
+}
+private: System::Void button4_Click(System::Object^ sender, System::EventArgs^ e) {
+	newWSDialog->Hide();
+}
+private: System::Void button5_Click(System::Object^ sender, System::EventArgs^ e) {
+	if (tbWorkSpaceName->Text == "")
+	{
+		MessageBox::Show("Please enter valid name", "Error", MessageBoxButtons::OK,MessageBoxIcon::Error);
+	}
+	else {
+		tbWorkSpaceName->Text = "";
+		newWSDialog->Hide();
+	}
+}
+private: System::Void textBox2_TextChanged(System::Object^ sender, System::EventArgs^ e) {
+
 }
 };
 }

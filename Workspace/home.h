c316299c0178a1c8276e58b3d0d5357e420b7b23
Ravi Kumar <ravikuk@outlook.com>
2021-05-29 13:25:36 +0530

    Added second panel

--- Workspace/home.h
+++ Workspace/home.h
@@ -21,6 +21,7 @@ namespace Workspace {
 			//
 			//TODO: Add the constructor code here
 			//
+			main->Hide();
 		}
 
 	protected:
@@ -39,6 +40,12 @@ namespace Workspace {
 	private: System::Windows::Forms::Button^ startButton;
 
 	private: System::Windows::Forms::PictureBox^ pictureBox1;
+	private: System::Windows::Forms::Panel^ main;
+	private: System::Windows::Forms::Label^ label2;
+	private: System::Windows::Forms::TextBox^ textBox1;
+	private: System::Windows::Forms::Button^ button2;
+	private: System::Windows::Forms::Button^ button1;
+	private: System::Windows::Forms::ListBox^ listBox1;
 	protected:
 
 	protected:
@@ -72,7 +79,14 @@ namespace Workspace {
 			this->label1 = (gcnew System::Windows::Forms::Label());
 			this->startButton = (gcnew System::Windows::Forms::Button());
 			this->pictureBox1 = (gcnew System::Windows::Forms::PictureBox());
+			this->main = (gcnew System::Windows::Forms::Panel());
+			this->label2 = (gcnew System::Windows::Forms::Label());
+			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
+			this->button2 = (gcnew System::Windows::Forms::Button());
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->listBox1 = (gcnew System::Windows::Forms::ListBox());
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->BeginInit();
+			this->main->SuspendLayout();
 			this->SuspendLayout();
 			// 
 			// close
@@ -103,9 +117,9 @@ namespace Workspace {
 			this->label1->ForeColor = System::Drawing::Color::White;
 			this->label1->Location = System::Drawing::Point(285, 146);
 			this->label1->Name = L"label1";
-			this->label1->Size = System::Drawing::Size(388, 89);
+			this->label1->Size = System::Drawing::Size(360, 89);
 			this->label1->TabIndex = 1;
-			this->label1->Text = L"Workspaces";
+			this->label1->Text = L"Workspace";
 			// 
 			// startButton
 			// 
@@ -123,6 +137,7 @@ namespace Workspace {
 			this->startButton->TabIndex = 2;
 			this->startButton->Text = L"Get Started";
 			this->startButton->UseVisualStyleBackColor = false;
+			this->startButton->Click += gcnew System::EventHandler(this, &home::startButton_Click);
 			// 
 			// pictureBox1
 			// 
@@ -136,11 +151,74 @@ namespace Workspace {
 			this->pictureBox1->TabStop = false;
 			this->pictureBox1->Click += gcnew System::EventHandler(this, &home::pictureBox1_Click);
 			// 
+			// main
+			// 
+			this->main->BackColor = System::Drawing::Color::Gainsboro;
+			this->main->BackgroundImage = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"main.BackgroundImage")));
+			this->main->Controls->Add(this->label2);
+			this->main->Controls->Add(this->textBox1);
+			this->main->Controls->Add(this->button2);
+			this->main->Controls->Add(this->button1);
+			this->main->Controls->Add(this->listBox1);
+			this->main->Dock = System::Windows::Forms::DockStyle::Fill;
+			this->main->Location = System::Drawing::Point(0, 0);
+			this->main->Name = L"main";
+			this->main->Size = System::Drawing::Size(900, 600);
+			this->main->TabIndex = 4;
+			// 
+			// label2
+			// 
+			this->label2->AutoSize = true;
+			this->label2->BackColor = System::Drawing::Color::Transparent;
+			this->label2->Font = (gcnew System::Drawing::Font(L"Segoe UI Semibold", 15, System::Drawing::FontStyle::Bold));
+			this->label2->ForeColor = System::Drawing::Color::Black;
+			this->label2->Location = System::Drawing::Point(332, 22);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(173, 35);
+			this->label2->TabIndex = 4;
+			this->label2->Text = L"Display Name";
+			// 
+			// textBox1
+			// 
+			this->textBox1->Font = (gcnew System::Drawing::Font(L"Segoe UI Semibold", 15, System::Drawing::FontStyle::Bold));
+			this->textBox1->Location = System::Drawing::Point(338, 92);
+			this->textBox1->Name = L"textBox1";
+			this->textBox1->Size = System::Drawing::Size(500, 41);
+			this->textBox1->TabIndex = 3;
+			// 
+			// button2
+			// 
+			this->button2->Location = System::Drawing::Point(192, 534);
+			this->button2->Name = L"button2";
+			this->button2->Size = System::Drawing::Size(145, 53);
+			this->button2->TabIndex = 2;
+			this->button2->Text = L"button2";
+			this->button2->UseVisualStyleBackColor = true;
+			// 
+			// button1
+			// 
+			this->button1->Location = System::Drawing::Point(19, 534);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(167, 53);
+			this->button1->TabIndex = 1;
+			this->button1->Text = L"button1";
+			this->button1->UseVisualStyleBackColor = true;
+			// 
+			// listBox1
+			// 
+			this->listBox1->FormattingEnabled = true;
+			this->listBox1->ItemHeight = 17;
+			this->listBox1->Location = System::Drawing::Point(19, 22);
+			this->listBox1->Name = L"listBox1";
+			this->listBox1->Size = System::Drawing::Size(281, 497);
+			this->listBox1->TabIndex = 0;
+			// 
 			// home
 			// 
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::None;
 			this->BackgroundImage = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"$this.BackgroundImage")));
 			this->ClientSize = System::Drawing::Size(900, 600);
+			this->Controls->Add(this->main);
 			this->Controls->Add(this->pictureBox1);
 			this->Controls->Add(this->startButton);
 			this->Controls->Add(this->label1);
@@ -153,6 +231,8 @@ namespace Workspace {
 			this->Text = L"home";
 			this->Load += gcnew System::EventHandler(this, &home::home_Load);
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->EndInit();
+			this->main->ResumeLayout(false);
+			this->main->PerformLayout();
 			this->ResumeLayout(false);
 			this->PerformLayout();
 
@@ -169,6 +249,9 @@ namespace Workspace {
 	}
 private: System::Void pictureBox1_Click(System::Object^ sender, System::EventArgs^ e) {
 	System::Diagnostics::Process::Start("https://alphaone.me");
+}
+private: System::Void startButton_Click(System::Object^ sender, System::EventArgs^ e) {
+	main->Show();
 }
 };
 }

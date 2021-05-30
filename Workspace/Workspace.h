00fa7e2a1bc99b51bd9d206e673d6210ce15dd77
Ravi Kumar <ravikuk@outlook.com>
2021-05-30 22:19:20 +0530

    New Class

---
+++ Workspace/Workspace.h
@@ -0,0 +1,18 @@
+#pragma once
+
+namespace Workspace {
+
+
+	using namespace System;
+	using namespace System::Collections;
+
+	public ref class Workspace {
+	public:	
+		Workspace(void) {
+			name = "";
+		}
+	private:
+		String^ name;
+		String^ name;
+	};
+}
--- Workspace/workspaces.h
+++ Workspace/workspaces.h
@@ -42,6 +42,9 @@ namespace Workspace {
 
 	private: System::Windows::Forms::Label^ labelWorkspaces;
 	private: System::Windows::Forms::TreeView^ treeView1;
+	private: System::Windows::Forms::Panel^ pNoWorkspace;
+	private: System::Windows::Forms::Label^ label2;
+	private: System::Windows::Forms::Label^ label1;
 
 	private:
 		/// <summary>
@@ -56,13 +59,16 @@ namespace Workspace {
 		/// </summary>
 		void InitializeComponent(void)
 		{
-			System::ComponentModel::ComponentResourceManager^ resources = (gcnew System::ComponentModel::ComponentResourceManager(workspaces::typeid));
 			this->panel1 = (gcnew System::Windows::Forms::Panel());
-			this->labelWorkspaces = (gcnew System::Windows::Forms::Label());
-			this->btAddWorkspace = (gcnew System::Windows::Forms::Button());
-			this->btAddItem = (gcnew System::Windows::Forms::Button());
 			this->treeView1 = (gcnew System::Windows::Forms::TreeView());
+			this->btAddItem = (gcnew System::Windows::Forms::Button());
+			this->btAddWorkspace = (gcnew System::Windows::Forms::Button());
+			this->labelWorkspaces = (gcnew System::Windows::Forms::Label());
+			this->pNoWorkspace = (gcnew System::Windows::Forms::Panel());
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->label2 = (gcnew System::Windows::Forms::Label());
 			this->panel1->SuspendLayout();
+			this->pNoWorkspace->SuspendLayout();
 			this->SuspendLayout();
 			// 
 			// panel1
@@ -77,26 +83,12 @@ namespace Workspace {
 			this->panel1->Size = System::Drawing::Size(250, 600);
 			this->panel1->TabIndex = 0;
 			// 
-			// labelWorkspaces
+			// treeView1
 			// 
-			this->labelWorkspaces->AutoSize = true;
-			this->labelWorkspaces->BackColor = System::Drawing::Color::Transparent;
-			this->labelWorkspaces->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 10));
-			this->labelWorkspaces->Location = System::Drawing::Point(3, 3);
-			this->labelWorkspaces->Name = L"labelWorkspaces";
-			this->labelWorkspaces->Size = System::Drawing::Size(102, 20);
-			this->labelWorkspaces->TabIndex = 0;
-			this->labelWorkspaces->Text = L"Workspaces";
-			// 
-			// btAddWorkspace
-			// 
-			this->btAddWorkspace->Anchor = System::Windows::Forms::AnchorStyles::Bottom;
-			this->btAddWorkspace->Location = System::Drawing::Point(3, 558);
-			this->btAddWorkspace->Name = L"btAddWorkspace";
-			this->btAddWorkspace->Size = System::Drawing::Size(123, 34);
-			this->btAddWorkspace->TabIndex = 1;
-			this->btAddWorkspace->Text = L"Add Workspace";
-			this->btAddWorkspace->UseVisualStyleBackColor = true;
+			this->treeView1->Location = System::Drawing::Point(8, 27);
+			this->treeView1->Name = L"treeView1";
+			this->treeView1->Size = System::Drawing::Size(231, 525);
+			this->treeView1->TabIndex = 3;
 			// 
 			// btAddItem
 			// 
@@ -109,19 +101,66 @@ namespace Workspace {
 			this->btAddItem->UseVisualStyleBackColor = true;
 			this->btAddItem->Click += gcnew System::EventHandler(this, &workspaces::button2_Click);
 			// 
-			// treeView1
+			// btAddWorkspace
 			// 
-			this->treeView1->Location = System::Drawing::Point(8, 27);
-			this->treeView1->Name = L"treeView1";
-			this->treeView1->Size = System::Drawing::Size(231, 525);
-			this->treeView1->TabIndex = 3;
+			this->btAddWorkspace->Anchor = System::Windows::Forms::AnchorStyles::Bottom;
+			this->btAddWorkspace->Location = System::Drawing::Point(3, 558);
+			this->btAddWorkspace->Name = L"btAddWorkspace";
+			this->btAddWorkspace->Size = System::Drawing::Size(123, 34);
+			this->btAddWorkspace->TabIndex = 1;
+			this->btAddWorkspace->Text = L"Add Workspace";
+			this->btAddWorkspace->UseVisualStyleBackColor = true;
+			// 
+			// labelWorkspaces
+			// 
+			this->labelWorkspaces->AutoSize = true;
+			this->labelWorkspaces->BackColor = System::Drawing::Color::Transparent;
+			this->labelWorkspaces->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 10));
+			this->labelWorkspaces->Location = System::Drawing::Point(3, 3);
+			this->labelWorkspaces->Name = L"labelWorkspaces";
+			this->labelWorkspaces->Size = System::Drawing::Size(102, 20);
+			this->labelWorkspaces->TabIndex = 0;
+			this->labelWorkspaces->Text = L"Workspaces";
+			// 
+			// pNoWorkspace
+			// 
+			this->pNoWorkspace->Controls->Add(this->label2);
+			this->pNoWorkspace->Controls->Add(this->label1);
+			this->pNoWorkspace->Location = System::Drawing::Point(257, 29);
+			this->pNoWorkspace->Name = L"pNoWorkspace";
+			this->pNoWorkspace->Size = System::Drawing::Size(631, 525);
+			this->pNoWorkspace->TabIndex = 1;
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 25));
+			this->label1->Location = System::Drawing::Point(198, 145);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(250, 48);
+			this->label1->TabIndex = 0;
+			this->label1->Text = L"Workspaces";
+			// 
+			// label2
+			// 
+			this->label2->AutoSize = true;
+			this->label2->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 10));
+			this->label2->ForeColor = System::Drawing::SystemColors::ActiveCaption;
+			this->label2->Location = System::Drawing::Point(225, 224);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(183, 20);
+			this->label2->TabIndex = 1;
+			this->label2->Text = L"No workspace selected";
 			// 
 			// workspaces
 			// 
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::None;
 			this->BackColor = System::Drawing::SystemColors::Control;
 			this->ClientSize = System::Drawing::Size(900, 600);
+			this->Controls->Add(this->pNoWorkspace);
 			this->Controls->Add(this->panel1);
+			this->Font = (gcnew System::Drawing::Font(L"Segoe UI", 7.8F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
 			this->FormBorderStyle = System::Windows::Forms::FormBorderStyle::None;
 			this->Name = L"workspaces";
 			this->StartPosition = System::Windows::Forms::FormStartPosition::CenterScreen;
@@ -129,6 +168,8 @@ namespace Workspace {
 			this->Load += gcnew System::EventHandler(this, &workspaces::workspaces_Load);
 			this->panel1->ResumeLayout(false);
 			this->panel1->PerformLayout();
+			this->pNoWorkspace->ResumeLayout(false);
+			this->pNoWorkspace->PerformLayout();
 			this->ResumeLayout(false);
 
 		}
